d29f646611d193cbb5a94f81ef0e9cdb6b14d162
T202410008992764 <zhoubencheng2022@163.com>
2024-04-25 18:06:03 +0800

    upgrade the README

--- os/src/stack_trace.rs
+++ os/src/stack_trace.rs
@@ -1,11 +1,11 @@
 use core::{arch::asm, ptr};
 
-pub unsafe fn print_stack_trace() -> (){
+pub unsafe fn print_stack_trace() -> (){ // 当前仅在panic发生时调用函数栈帧打印
     let mut fp: *const usize;
     asm!("mv {}, fp", out(reg) fp); // 将fp寄存器(汇编代码内)的值赋给fp变量（汇编代码外的高级语言参数）
     println!("=== Begin stack trace ===");
     while fp != ptr::null() {
-        let saved_ra = *fp.sub(1);
+        let saved_ra = *fp.sub(1); // 注意，这里 *const T类型的sub()方法的优先级高于 * 解引用的优先级，因此是先降低地址再解引用
         let saved_fp = *fp.sub(2);
         println!("ra = {:16x}, fp = {:16x}", saved_ra, saved_fp);
         fp = saved_fp as *const usize;
